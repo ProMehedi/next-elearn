19919f1c6a255d5b530f8c6134fd55a2ee1c0a44
promehedi <mmhasan563@gmail.com>
2021-05-30 21:52:38 +0600

    Setup Server

---
+++ server/server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import cors from 'cors'
+import colors from 'colors'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+
+// Enable .env
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(express.json())
+
+// Define Variable
+const API_URL = process.env.API_URL || '/api'
+const PORT = process.env.PORT || 5000
+const NODE_ENV = process.env.NODE_ENV
+
+app.get('/', (req, res) => {
+  res.send('API is running..')
+})
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`.yellow.bold)
+)
