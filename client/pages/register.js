2f1d7bae6ad9d210f41345326021e9aa50b29799
promehedi <mmhasan563@gmail.com>
2021-05-30 23:43:43 +0600

    Register New User

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import axios from 'axios'
-import { Col, Container, Row, Card, Form, Button } from 'react-bootstrap'
+import { Col, Container, Row, Card, Form, Button, Alert } from 'react-bootstrap'
 import Jumbotron from '../components/Jumbotron'
 
 const RegisterPage = () => {
@@ -8,14 +8,43 @@ const RegisterPage = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPass, setConfirmPass] = useState('')
+  const [message, setMessage] = useState('')
 
   const handleSubmit = async (e) => {
     e.preventDefault()
+
+    // Validations
+    if (!name) {
+      setMessage('Name is required!')
+      return false
+    }
+    if (!email) {
+      setMessage('Email is required!')
+      return false
+    }
+    if (!password) {
+      setMessage('Password is required!')
+      return false
+    }
+    if (password.length < 6) {
+      setMessage("Password can't be less than 6 characters!")
+      return false
+    }
+    if (password.length > 65) {
+      setMessage("Password can't be longer than 65 characters!")
+      return false
+    }
+    if (password != confirmPass) {
+      setMessage('Password not match!')
+      return false
+    }
+
     const { data } = await axios.post(`http://localhost:5000/api/users`, {
       name,
       email,
       password,
     })
+    setMessage('')
     console.log(data)
   }
 
@@ -28,7 +57,8 @@ const RegisterPage = () => {
             <Card>
               <Card.Body>
                 <Form onSubmit={handleSubmit}>
-                  <Form.Group className='mb-3'>
+                  <Form.Group className='mb-3' controlId='name'>
+                    <Form.Label>Your Name</Form.Label>
                     <Form.Control
                       placeholder='Enter Name'
                       type='text'
@@ -36,7 +66,8 @@ const RegisterPage = () => {
                       onChange={(e) => setName(e.target.value)}
                     />
                   </Form.Group>
-                  <Form.Group className='mb-3'>
+                  <Form.Group className='mb-3' controlId='email'>
+                    <Form.Label>Your Email</Form.Label>
                     <Form.Control
                       placeholder='Enter Email'
                       type='email'
@@ -44,7 +75,8 @@ const RegisterPage = () => {
                       onChange={(e) => setEmail(e.target.value)}
                     />
                   </Form.Group>
-                  <Form.Group className='mb-3'>
+                  <Form.Group className='mb-3' controlId='password'>
+                    <Form.Label>Password</Form.Label>
                     <Form.Control
                       placeholder='Enter Password'
                       type='password'
@@ -52,7 +84,8 @@ const RegisterPage = () => {
                       onChange={(e) => setPassword(e.target.value)}
                     />
                   </Form.Group>
-                  <Form.Group className='mb-3'>
+                  <Form.Group className='mb-3' controlId='confirmPass'>
+                    <Form.Label>Confirm Password</Form.Label>
                     <Form.Control
                       placeholder='Confirm Password'
                       type='password'
@@ -66,6 +99,11 @@ const RegisterPage = () => {
                 </Form>
               </Card.Body>
             </Card>
+            {message && (
+              <div className='my-3'>
+                <Alert variant='danger'>{message}</Alert>
+              </div>
+            )}
           </Col>
         </Row>
       </Container>
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,15 +1,30 @@
 import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+import generateToken from '../utils/genarateToken.js'
 
 // @desc    Auth User & Get Token
 // @route   GET /api/v1/users/login
 // @access  Puclic
 export const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
+  const userExists = await User.findOne({ email })
 
-  if (req.body) {
-    res.status(201).json({ name, email, password })
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({ name, email, password })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    })
   } else {
-    res.status(401)
-    throw new Error('Invalid email or password')
+    res.status(400)
+    throw new Error('Invalid user data')
   }
 })
